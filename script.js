15a819b9e9cffcc8a203f21f7b31419a0a6ef53c
Joe Mou <dev@mou.fo>
2025-11-04 15:17:52 -0500

    YOGOLO
    You Only Game of Life Once

---
+++ script.js
@@ -0,0 +1,63 @@
+const BOARD_WIDTH = 100;
+const BOARD_HEIGHT = 50;
+const BOARD_ELEMENTS = BOARD_HEIGHT * BOARD_WIDTH
+
+function newBoard() { return Array.from({length: BOARD_ELEMENTS}).map(() => false); }
+
+function getIndex(x, y) {
+  if (x < 0) x += BOARD_WIDTH;
+  if (x > BOARD_WIDTH) x -= BOARD_WIDTH
+  if (y < 0) y += BOARD_HEIGHT;
+  if (y > BOARD_HEIGHT) y -= BOARD_HEIGHT
+  return y * BOARD_WIDTH + x;
+}
+
+function get(board, x, y) {
+  return board[getIndex(x, y)];
+}
+
+function set(board, x, y, value) {
+  board[getIndex(x, y)] = value;
+}
+
+function countNeighbors(board, x, y) {
+  let count = 0;
+  for (const dx of [-1, 0, 1]) {
+    for (const dy of [-1, 0, 1]) {
+      if (dx === 0 && dy === 0) continue;
+      if (get(board, x + dx, y + dy)) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+function deadOrAlive(state, count) {
+  if (state) {  // alive
+    if (count < 2) return false;
+    if (count < 4) return true;
+    return false;
+  } else {  // dead
+    return count === 3;
+  }
+}
+
+function updateBoard(board) {
+  const nextBoard = newBoard();
+  for (let x = 0; x < BOARD_WIDTH; x++) {
+    for (let y = 0; y < BOARD_HEIGHT; y++) {
+     const count = countNeighbors(board, x, y);
+     set(nextBoard, x, y, deadOrAlive(get(board, x, y), count));
+    }
+  }
+}
+
+const lastBoard = newBoard();
+const currentBoard = newBoard();
+
+function gameLoop() {
+  while (true) {
+    currentBoard = updateBoard(lastBoard);
+  }
+}
